3112eb7beaeb1c585f62e185eb0686e9cb6edc4a
saicharan-o <saicharanr227@gmail.com>
2026-01-18 15:32:01 +0530

    Linear-Regression

---
+++ Linear-Regression.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+d=pd.read_csv("house_price_single_linear_regression.csv")
+plt.xlabel("Area_sqft")
+plt.ylabel("Price")
+plt.scatter(d.Area_sqft,d.Price,color="r",marker=".")
+plt.show()
+x=d[["Area_sqft"]]
+y=d[["Price"]]
+md=LinearRegression()
+md.fit(x,y)
+print("Slope: \n", md.coef_)
+print("Intercept: \n", md.intercept_)
+print("Prediction 1: ",md.predict(pd.DataFrame({"Area_sqft":[2030]})))
+print("Prediction 2: ",md.predict(pd.DataFrame({"Area_sqft":[48202]})))

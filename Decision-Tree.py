b226977ba36b793e146bb482e7aaff65912c871c
saicharan-o <saicharanr227@gmail.com>
2026-04-05 13:00:46 +0530

    ADDED new Function in   Decision-Tree

--- Decision-Tree.py
+++ Decision-Tree.py
@@ -30,68 +30,16 @@ def decode_input(company, job, degree, exp):
         "degree": l_d.inverse_transform([degree])[0],
         "experience": l_e.inverse_transform([exp])[0]
     }
+def print_prediction(title, company, job, degree, exp, pred):
+    info = decode_input(company, job, degree, exp)
+    salary = decode_salary(pred)
 
-# Prediction 2
-data = {"company_n":[1],"job_n":[1],"degree_n":[0],"experience_n":[0]}
-p2 = md.predict(pd.DataFrame(data))
-info = decode_input(1,1,0,0)
+    print(f"{title}")
+    print(f"Company     : {info['company'].title()}")
+    print(f"Role        : {info['job'].title()}")
+    print(f"Degree      : {info['degree'].title()}")
+    print(f"Experience  : {info['experience'].title()}")
+    print(f"Prediction  : {salary}")
+    print("-" * 40)
 
-print("Prediction 2:")
-print("Input:", info)
-print("Salary:", decode_salary(p2[0]))
-print()
-
-
-# Prediction 3
-data = {"company_n":[2],"job_n":[2],"degree_n":[2],"experience_n":[1]}
-p3 = md.predict(pd.DataFrame(data))
-info = decode_input(2,2,2,1)
-
-print("Prediction 3:")
-print("Input:", info)
-print("Salary:", decode_salary(p3[0]))
-print()
-
-
-# Prediction 4
-data = {"company_n":[1],"job_n":[2],"degree_n":[0],"experience_n":[1]}
-p4 = md.predict(pd.DataFrame(data))
-info = decode_input(1,2,0,1)
-
-print("Prediction 4:")
-print("Input:", info)
-print("Salary:", decode_salary(p4[0]))
-print()
-
-
-# Prediction 5
-data = {"company_n":[0],"job_n":[1],"degree_n":[2],"experience_n":[1]}
-p5 = md.predict(pd.DataFrame(data))
-info = decode_input(0,1,2,1)
-
-print("Prediction 5:")
-print("Input:", info)
-print("Salary:", decode_salary(p5[0]))
-print()
-
-
-# Prediction 6
-data = {"company_n":[2],"job_n":[2],"degree_n":[1],"experience_n":[1]}
-p6 = md.predict(pd.DataFrame(data))
-info = decode_input(2,2,1,1)
-
-print("Prediction 6:")
-print("Input:", info)
-print("Salary:", decode_salary(p6[0]))
-print()
-
-
-# Prediction 7
-data = {"company_n":[1],"job_n":[2],"degree_n":[0],"experience_n":[1]}
-p7 = md.predict(pd.DataFrame(data))
-info = decode_input(1,2,0,1)
-
-print("Prediction 7:")
-print("Input:", info)
-print("Salary:", decode_salary(p7[0]))
-print()
+    

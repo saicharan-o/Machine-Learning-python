63789d47320a82b251dccf83a42aa112496e2dc5
saicharan-o <saicharanr227@gmail.com>
2026-04-05 13:09:40 +0530

    Updates Decision_Tree File

--- Decision-Tree.py
+++ Decision-Tree.py
@@ -7,14 +7,19 @@ l_c=LabelEncoder()
 l_j=LabelEncoder()
 l_d=LabelEncoder()
 l_e=LabelEncoder()
+
 x["company_n"]=l_c.fit_transform(x["company"])
 x["job_n"]=l_j.fit_transform(x["job"])
 x["degree_n"]=l_d.fit_transform(x["degree"])
 x["experience_n"]=l_e.fit_transform(x["experience"])
+
 print(x.head())
+
 x_n=x.drop(x[["company","job","degree","experience"]],axis="columns")
+
 print(x_n)
 print(y)
+
 from sklearn.tree import DecisionTreeClassifier
 md=DecisionTreeClassifier()
 md.fit(x_n,y)

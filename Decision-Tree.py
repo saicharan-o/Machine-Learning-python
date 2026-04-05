9458a9c1fef5fd19be8091b4f61266c2c2c916e3
saicharan-o <saicharanr227@gmail.com>
2026-04-05 13:01:34 +0530

    ADDED Predictions in Decision-Tree

--- Decision-Tree.py
+++ Decision-Tree.py
@@ -30,6 +30,7 @@ def decode_input(company, job, degree, exp):
         "degree": l_d.inverse_transform([degree])[0],
         "experience": l_e.inverse_transform([exp])[0]
     }
+
 def print_prediction(title, company, job, degree, exp, pred):
     info = decode_input(company, job, degree, exp)
     salary = decode_salary(pred)
@@ -42,4 +43,26 @@ def print_prediction(title, company, job, degree, exp, pred):
     print(f"Prediction  : {salary}")
     print("-" * 40)
 
-    
+# Prediction 2
+p2 = md.predict(pd.DataFrame({"company_n":[1],"job_n":[1],"degree_n":[0],"experience_n":[0]}))
+print_prediction("Prediction 2", 1,1,0,0, p2[0])
+
+# Prediction 3
+p3 = md.predict(pd.DataFrame({"company_n":[2],"job_n":[2],"degree_n":[2],"experience_n":[1]}))
+print_prediction("Prediction 3", 2,2,2,1, p3[0])
+
+# Prediction 4
+p4 = md.predict(pd.DataFrame({"company_n":[1],"job_n":[2],"degree_n":[0],"experience_n":[1]}))
+print_prediction("Prediction 4", 1,2,0,1, p4[0])
+
+# Prediction 5
+p5 = md.predict(pd.DataFrame({"company_n":[0],"job_n":[1],"degree_n":[2],"experience_n":[1]}))
+print_prediction("Prediction 5", 0,1,2,1, p5[0])
+
+# Prediction 6
+p6 = md.predict(pd.DataFrame({"company_n":[2],"job_n":[2],"degree_n":[1],"experience_n":[1]}))
+print_prediction("Prediction 6", 2,2,1,1, p6[0])
+
+# Prediction 7
+p7 = md.predict(pd.DataFrame({"company_n":[1],"job_n":[2],"degree_n":[0],"experience_n":[1]}))
+print_prediction("Prediction 7", 1,2,0,1, p7[0])    

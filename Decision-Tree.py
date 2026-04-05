bc0a6e726ab6c7092d6db417815fb8278bbe6057
saicharan-o <saicharanr227@gmail.com>
2026-04-05 12:51:07 +0530

    Updated Decision-Tree

--- Decision-Tree.py
+++ Decision-Tree.py
@@ -19,10 +19,23 @@ from sklearn.tree import DecisionTreeClassifier
 md=DecisionTreeClassifier()
 md.fit(x_n,y)
 print(md.score(x_n,y))
-print("Prediction 1: ",md.predict(pd.DataFrame({"company_n":[0],"job_n":[2],"degree_n":[1],"experience_n":[0]})))
-print("Prediction 2: ",md.predict(pd.DataFrame({"company_n":[4],"job_n":[1],"degree_n":[0],"experience_n":[0]})))
-print("Prediction 3: ",md.predict(pd.DataFrame({"company_n":[2],"job_n":[2],"degree_n":[2],"experience_n":[1]})))
-print("Prediction 4: ",md.predict(pd.DataFrame({"company_n":[4],"job_n":[2],"degree_n":[0],"experience_n":[1]})))
-print("Prediction 5: ",md.predict(pd.DataFrame({"company_n":[0],"job_n":[1],"degree_n":[2],"experience_n":[1]})))
-print("Prediction 6: ",md.predict(pd.DataFrame({"company_n":[2],"job_n":[2],"degree_n":[1],"experience_n":[1]})))
-print("Prediction 7: ",md.predict(pd.DataFrame({"company_n":[3],"job_n":[2],"degree_n":[0],"experience_n":[1]})))
+def decode_salary(val):
+    return ">100K" if val == 1 else "<=100K"
+
+p2 = md.predict(pd.DataFrame({"company_n":[1],"job_n":[1],"degree_n":[0],"experience_n":[0]}))
+print("Prediction 2:", decode_salary(p2[0]))
+
+p3 = md.predict(pd.DataFrame({"company_n":[2],"job_n":[2],"degree_n":[2],"experience_n":[1]}))
+print("Prediction 3:", decode_salary(p3[0]))
+
+p4 = md.predict(pd.DataFrame({"company_n":[1],"job_n":[2],"degree_n":[0],"experience_n":[1]}))
+print("Prediction 4:", decode_salary(p4[0]))
+
+p5 = md.predict(pd.DataFrame({"company_n":[0],"job_n":[1],"degree_n":[2],"experience_n":[1]}))
+print("Prediction 5:", decode_salary(p5[0]))
+
+p6 = md.predict(pd.DataFrame({"company_n":[2],"job_n":[2],"degree_n":[1],"experience_n":[1]}))
+print("Prediction 6:", decode_salary(p6[0]))
+
+p7 = md.predict(pd.DataFrame({"company_n":[1],"job_n":[2],"degree_n":[0],"experience_n":[1]}))
+print("Prediction 7:", decode_salary(p7[0]))

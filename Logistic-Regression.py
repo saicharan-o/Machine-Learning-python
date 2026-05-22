440de7601d06c2817a65a5f44d12e664a5940f0a
saicharan-r02 <saicharanr227@gmail.com>
2026-05-22 14:04:48 +0530

    Adding few lines in L-R file

--- Logistic-Regression.py
+++ Logistic-Regression.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
 d=pd.read_csv("Hours.csv")
 print(d)
+
 plt.scatter(d.Overtime_Hours,d.Left_Company,marker='+',color='black')
 plt.show()
 from sklearn.model_selection import train_test_split
